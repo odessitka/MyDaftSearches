618a4403a009acfd50cbe498f0437b438a621707
odessitka <odessitka@list.ru>
2017-05-01 15:13:37 +0100

    add get_distance function

--- maps.py
+++ maps.py
@@ -3,15 +3,16 @@ import time
 
 import googlemaps
 # import responses
+def get_distance(origin_address, destination_address):
+    gmaps = googlemaps.Client(key='AIzaSyCpqCdiOg69YK9-tf2YOPLXRVXGxPuabPk')
+    # origins = ["o'connell bridge, dublin"]
+    # destinations = ["oneview, blackrock"]
+    matrix = gmaps.distance_matrix(origin_address, destination_address)
+    # print (matrix)
+    # print (type(matrix['rows'][0]))
+    # print (matrix['rows'][0])
+    # #elements = {matrix["rows"][0]}
+    # print(matrix["rows"][0]["elements"][0]["distance"]["value"])
+    return matrix["rows"][0]["elements"][0]["distance"]["value"]
 
-gmaps = googlemaps.Client(key='AIzaSyCpqCdiOg69YK9-tf2YOPLXRVXGxPuabPk')
-origins = ["o'connell bridge, dublin"]
-destinations = ["oneview, blackrock"]
-
-matrix = gmaps.distance_matrix(origins, destinations)
-print (matrix)
-print(type(matrix))
-print(matrix['destination_addresses'])
-print(type(matrix['destination_addresses']))
-print(matrix['destination_addresses'][0])
-
+get_distance("o'connell bridge, dublin","oneview, blackrock")
