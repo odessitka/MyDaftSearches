4c3b7d35f3d25941db58bfe0c9073148c18a2e25
ppnevmatikatos <pnevmap@gmail.com>
2017-05-03 23:44:10 +0100

    file move, rename

--- HelperClasses.py
+++ HelperClasses.py
@@ -1,17 +1,11 @@
 import googlemaps
 
 
-def get_distance_duration_matrix(destination_address, origin_address):
-    gmaps = googlemaps.Client(key='AIzaSyCpqCdiOg69YK9-tf2YOPLXRVXGxPuabPk')
-    matrix = gmaps.distance_matrix(origin_address, destination_address, transit_mode="walking")
-    return matrix["rows"][0]["elements"][0]["distance"]["value"], matrix["rows"][0]["elements"][0]["duration"]["value"]
-
-
-
-
 class CommuteMatrix:
     def __init__(self, destination_address, origin_address):
-        matrix = get_distance_duration_matrix(destination_address, origin_address)
+        self.destination_address = destination_address
+        self.origin_address = origin_address
+        matrix = self.get_distance_duration_matrix()
         self.__distance = matrix[0]
         self.__duration = matrix[1]
 
@@ -23,8 +17,13 @@ class CommuteMatrix:
     def duration(self):
         return self.__duration
 
+    def get_distance_duration_matrix(self):
+        gmaps = googlemaps.Client(key='AIzaSyCpqCdiOg69YK9-tf2YOPLXRVXGxPuabPk')
+        matrix = gmaps.distance_matrix(self.origin_address, self.destination_address, transit_mode="walking")
+        return matrix["rows"][0]["elements"][0]["distance"]["value"], matrix["rows"][0]["elements"][0]["duration"]["value"]
 
-class DetailsHouse:
+
+class House:
     def __init__(self, box):
         ad_a = box.find("a")
         address_full = ad_a.text
--- daft_search.py
+++ daft_search.py
@@ -1,7 +1,7 @@
 import urllib, re, sqlite3, googlemaps
 from bs4 import BeautifulSoup
 import dbhelpers
-import maps
+import HelperClasses
 import webbrowser
 import os
 
@@ -28,7 +28,7 @@ def create_report():
 def calc_distances():
     addresses = dbhelpers.get_addresses()
     for (address, id) in addresses:
-        matrix = maps.CommuteMatrix(address, "oneview, blackrock")
+        matrix = HelperClasses.CommuteMatrix(address, "oneview, blackrock")
         dbhelpers.update_house(id, matrix.distance, matrix.duration)
 
 
@@ -36,20 +36,10 @@ def extract_and_insert():
     # Extract data from daft, clear it and insert into database
     url = "http://www.daft.ie/dublin-city/residential-property-for-rent/blackrock,booterstown,dun-laoghaire,monkstown,sandymount/?s%5Bmxp%5D=1850&s%5Badvanced%5D=1&rental_tab_name=advanced-sf&searchSource=rental"
     html_of_search = urllib.request.urlopen(url).read()
-    # print(type(html_of_search))
     soup_of_search = BeautifulSoup(html_of_search, "lxml")
     boxes = soup_of_search.find_all('div', "box")
-    # if len(boxes) > 0:
     for box in boxes:
-        house = maps.DetailsHouse(box)
-        # ad_a = box.find("a")
-        # address_full = ad_a.text
-        # add = address_full.split("-")
-        # address = add[0].strip()
-        # url = ('http://www.daft.ie' + ad_a.get("href"))
-        # get_image = box.find("img")
-        # image = get_image.get("data-original")
-        # price = box.find("strong", "price").text
+        house = HelperClasses.House(box)
         dbhelpers.insert_house(house.price, house.address, house.url, house.image)
 
 
