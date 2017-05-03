3d9f0323ea26e47fd4881704c4a4efa16c730f77
odessitka <odessitka@list.ru>
2017-05-03 13:29:59 +0100

    refactoring

--- daft_search.py
+++ daft_search.py
@@ -34,7 +34,7 @@ def calc_distances():
 
 def extract_and_insert():
     # Extract data from daft, clear it and insert into database
-    url = "http://www.daft.ie/dublin-city/residential-property-for-rent/booterstown,dun-laoghaire,monkstown,sandymount/?s%5Bmxp%5D=1850&s%5Badvanced%5D=1&rental_tab_name=advanced-sf&searchSource=rental"
+    url = "http://www.daft.ie/dublin-city/residential-property-for-rent/blackrock,booterstown,dun-laoghaire,monkstown,sandymount/?s%5Bmxp%5D=1850&s%5Badvanced%5D=1&rental_tab_name=advanced-sf&searchSource=rental"
     html_of_search = urllib.request.urlopen(url).read()
     # print(type(html_of_search))
     soup_of_search = BeautifulSoup(html_of_search, "lxml")
@@ -57,10 +57,15 @@ def compose_html(houses):
     <head></head>
     <body>
     <h1> Houses close to Oneview Healthcare </h1>
-    <table border="1" width=90%>
-    <tr><th>Time</th><th>Address</th><th>Price</th><th>Link</th></tr>"""
+    <table border="1" width=93%>
+    <colgroup>
+        <col style="background-color:#E0F2F7">
+        <col style="background-color:#F8E0EC">
+        <col style="background-color:#E0F8E0">
+    </colgroup>
+    <tr><th>Time</th><th>Address</th><th>Price</th></tr>"""
     for house in houses:
-        message += "<tr><td>" + str(house[0]) + "</td><td>" + house[1] + "</td><td>" + house[2] + "</td><td>" + house[3] + "</td></tr>"
+        message += "<tr><td>{a}</td><td><a href={d}>{b}</a></td><td>{c}</td><tr>".format(a=str(house[0]), b=house[1], c=house[2], d=house[3])
     message += """</table>
     </body>
     </html>"""
